0b7ea197247c637c79e4beb4da6d5d9190300ec7
ushironoko <apple19940820@yahoo.co.jp>
2018-09-16 21:40:44 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# aANDgo
+
+超A&G recording with RTMPdump from Go(exec)
+
+# Use
+
+```
+git clone
+```
+
+Arrangement
+
+* rtmpdump.exe or rtmpdump install(yum,apt-get...etc)
+* setting.json
+  * recTime
+  * output dir
+  * rtmp url
+
+Go Command Run
+```
+go run main.go
+
+or
+
+go build main.go
+```
+
+Operation check
---
+++ main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os/exec"
+	"time"
+
+	"github.com/mattn/go-shellwords"
+)
+
+//Setting : 録画設定json用構造体
+type Setting struct {
+	RecTime       string `json:"recTime"`
+	SaveDir       string `json:"saveDir"`
+	ConnectServer string `json:"connectServer"`
+}
+
+func runCmdStr(cmdstr string) error {
+
+	log.Println(cmdstr)
+
+	//文字列をコマンドとオプションにスライス
+	c, err := shellwords.Parse(cmdstr)
+	if err != nil {
+		failOnError(err)
+		return err
+	}
+
+	switch len(c) {
+	case 0:
+		//空のコマンドの場合
+		return nil
+	case 1:
+		//オプションなしコマンド
+		err := exec.Command(c[0]).Run()
+		if err != nil {
+			failOnError(err)
+			return err
+		}
+	default:
+		//コマンド+オプション
+		err := exec.Command(c[0], c[1:]...).Run()
+		if err != nil {
+			failOnError(err)
+			return err
+		}
+	}
+
+	return nil
+}
+
+func failOnError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	//setting.json 読み込み
+	bytes, err := ioutil.ReadFile("setting.json")
+	if err != nil {
+		failOnError(err)
+	}
+
+	//json デコード
+	var setting []Setting
+	if err := json.Unmarshal(bytes, &setting); err != nil {
+		failOnError(err)
+	}
+
+	//ファイル名生成
+	t := time.Now()
+	fileName := t.Format("2006_01_02_15_04_05") + ".flv"
+
+	//コマンド
+	cmdstr := "rtmpdump --live --rtmp " + setting[0].ConnectServer + " --timeout 60 -B " + setting[0].RecTime + " -o " + setting[0].SaveDir + fileName
+
+	//実行
+	runCmdStr(cmdstr)
+
+}
