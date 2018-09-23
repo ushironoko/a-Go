1399f5a96675adbf9e3b0f7dff0b8dea1de1340a
ushironoko <apple19940820@yahoo.co.jp>
2018-09-23 23:32:27 +0900

    comment trim

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	"github.com/mattn/go-shellwords"
 )
 
-//Setting : 録画設定json用構造体
+//Setting setting.json struct
 type Setting struct {
 	StartHour     int    `json:"startHour"`
 	RecTime       string `json:"recTime"`
@@ -22,21 +22,18 @@ func runCmdStr(cmdstr string) error {
 
 	log.Println(cmdstr)
 
-	//文字列をコマンドとオプションにスライス
 	c, err := shellwords.Parse(cmdstr)
 	failOnError(err)
 
 	switch len(c) {
 	case 0:
-		//空のコマンドの場合
 		return nil
+
 	case 1:
-		//オプションなしコマンド
 		err := exec.Command(c[0]).Run()
 		failOnError(err)
 
 	default:
-		//コマンド+オプション
 		err := exec.Command(c[0], c[1:]...).Run()
 		failOnError(err)
 
@@ -52,30 +49,24 @@ func failOnError(err error) {
 }
 
 func main() {
-	//setting.json 読み込み
+
 	bytes, err := ioutil.ReadFile("setting.json")
 	failOnError(err)
 
-	//json デコード
 	var setting []Setting
 	if err := json.Unmarshal(bytes, &setting); err != nil {
 		failOnError(err)
 	}
 
-	//現在時刻取得
 	t := time.Now()
 	hour := t.Hour()
 
-	//ファイル名生成
 	fileName := t.Format("2006_01_02_15_04_05") + ".flv"
 
-	//スケジュールを読み込んで録画スタート
 	for _, schedule := range setting {
 		if schedule.StartHour == hour {
-			//コマンド
 			cmdstr := "rtmpdump --live --rtmp " + schedule.ConnectServer + " --timeout 60 -B " + schedule.RecTime + " -o " + schedule.OutputDir + fileName
 
-			//実行
 			runCmdStr(cmdstr)
 
 			break
